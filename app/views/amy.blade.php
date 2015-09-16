c5a631defac5e185aebe964f0e57915f9c2b0147
James <jfefes@gmail.com>
2015-09-16 13:50:03 -0400

    the actor's act

--- app/views/amy.blade.php
+++ app/views/amy.blade.php
@@ -9,8 +9,12 @@
 @section('content')
 
 
-    <div id="doc">
         <h1>Amy: The Actor's Act</h1>
+
+        <div class="row">
+          <div id="doc">
+
+        </div>
         <!-- JS here to prevent 'flash' of all the default audio players -->
         <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js"></script>
         <script type="text/javascript" charset="utf-8">
@@ -18,7 +22,7 @@
                 $("audio").removeAttr("controls").each(function(i, audioElement) {
                     var audio = $(this);
                     var that = this; //closure to keep reference to current audio tag
-                    $("#doc").append($('<button class="btn btn-default" style="margin:10px;">'+audio.attr("title")+'</button>').click(function() {
+                    $("#doc").append($('<button class="btn btn-default col-xs-6" style="margin:10px;">'+audio.attr("title")+'</button>').click(function() {
                         that.play();
                     }));
                 });
